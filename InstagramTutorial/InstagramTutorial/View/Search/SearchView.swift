9dbdce5a710bb128cd8c6dd19a32d6929d8d5495
song1900 <woojin1900@gmail.com>
2021-07-04 22:48:52 +0900

    Building The PostGridView

--- InstagramTutorial/InstagramTutorial/View/Search/SearchView.swift
+++ InstagramTutorial/InstagramTutorial/View/Search/SearchView.swift
@@ -18,7 +18,7 @@ struct SearchView: View {
             SearchBar(text: $searchText)
                 .padding()
             
-            // grid view / user list view
+            PostGridView()
         }
         
         
