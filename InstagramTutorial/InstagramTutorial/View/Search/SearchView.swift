cc598b1dfd4dd616e7b52e6198389086d53bccd9
song1900 <woojin1900@gmail.com>
2021-07-04 22:39:22 +0900

    Building a Search Bar

--- InstagramTutorial/InstagramTutorial/View/Search/SearchView.swift
+++ InstagramTutorial/InstagramTutorial/View/Search/SearchView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State var searchText = ""
+    
     var body: some View {
-        Text("Search")
+        
+        ScrollView {
+            
+            SearchBar(text: $searchText)
+                .padding()
+            
+            // grid view / user list view
+        }
+        
+        
     }
 }
 
