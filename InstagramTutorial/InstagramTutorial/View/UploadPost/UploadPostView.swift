21a32c576d4249a50172b9318eabad2467edb287
song1900 <woojin1900@gmail.com>
2021-07-10 21:50:29 +0900

    Building the User Interface

--- InstagramTutorial/InstagramTutorial/View/UploadPost/UploadPostView.swift
+++ InstagramTutorial/InstagramTutorial/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+    
     var body: some View {
-        Text("Upload Post")
+        VStack {
+            if postImage == nil {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Photo +")
+                        .font(.system(size: 45, weight: .bold))
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                })
+            } else {
+                HStack(alignment: .top) {
+                    Image("batman")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption..", text: $captionText)
+                }.padding()
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }).padding()
+                
+            }
+            
+            Spacer()
+            
+        }
     }
 }
 
