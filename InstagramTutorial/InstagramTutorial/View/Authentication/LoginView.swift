f50151e88b2efe30468c66226f9b9db244ae5a7f
song1900 <woojin1900@gmail.com>
2021-07-17 19:55:01 +0900

    Feat : Login View 마무리

--- InstagramTutorial/InstagramTutorial/View/Authentication/LoginView.swift
+++ InstagramTutorial/InstagramTutorial/View/Authentication/LoginView.swift
@@ -23,30 +23,62 @@ struct LoginView: View {
                         .font(.system(size: 25, weight: .semibold))
                     
                     
-                    // email field
-                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 32)
+                    VStack(spacing: 20) {
+
+                        CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 32)
+                        
+                        CustomSecureField(text: $password, placeholder: Text("Passowrd"))
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 32)
+                    }
                     
-                    // password field
-                    CustomSecureField(text: $password, placeholder: Text("Passowrd"))
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 32)
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Forgot Password?")
+                                .font(.system(size: 13, weight: .semibold))
+                                .foregroundColor(.white)
+                                .padding(.top)
+                                .padding(.trailing, 28)
+                        })
+                    }
                     
-                    // forgot password
                     
-                    // sign in
                     
-                    // go to sign up
-                    
+                    Button(action: {}, label: {
+                        Text("Sign in")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 360, height: 50)
+                            .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                            .clipShape(Capsule())
+                            .padding()
+                    })
                     
                     Spacer()
+                    
+                    Button(action: {}, label: {
+                        HStack {
+                            Text("Don't have an account?")
+                                .font(.system(size: 14))
+                            
+                            Text("Sign Up")
+                                .font(.system(size: 14, weight: .semibold))
+                        }.foregroundColor(.white)
+                    })
+                    
+                    
                         
                 }
             }
