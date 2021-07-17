ef7582ff2ad42eed5aa15abf0006d85422a5fe0f
song1900 <woojin1900@gmail.com>
2021-07-17 21:08:30 +0900

    Building The Registration View

--- InstagramTutorial/InstagramTutorial/View/Authentication/LoginView.swift
+++ InstagramTutorial/InstagramTutorial/View/Authentication/LoginView.swift
@@ -68,19 +68,21 @@ struct LoginView: View {
                     
                     Spacer()
                     
-                    Button(action: {}, label: {
-                        HStack {
-                            Text("Don't have an account?")
-                                .font(.system(size: 14))
-                            
-                            Text("Sign Up")
-                                .font(.system(size: 14, weight: .semibold))
-                        }.foregroundColor(.white)
-                    })
-                    
-                    
-                        
+                    NavigationLink(
+                        destination: RegistrationView()
+                            .navigationBarHidden(true),
+                        label: {
+                            HStack {
+                                Text("Don't have an account?")
+                                    .font(.system(size: 14))
+                                
+                                Text("Sign Up")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }.foregroundColor(.white)
+                        }).padding(.bottom, 16)
+                
                 }
+                .padding(.top, -44)
             }
         }
     }
--- InstagramTutorial/InstagramTutorial/View/Authentication/RegistrationView.swift
+++ InstagramTutorial/InstagramTutorial/View/Authentication/RegistrationView.swift
@@ -8,8 +8,90 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var fullname = ""
+    @State private var username = ""
+    @State private var password = ""
+    @Environment(\.presentationMode) var mode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("+\nPhoto")
+                        .font(.system(size: 45, weight: .bold))
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                }).padding()
+                
+                
+                VStack(spacing: 20) {
+
+                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 32)
+                    
+                    CustomTextField(text: $email, placeholder: Text("Username"), imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 32)
+                    
+                    CustomTextField(text: $email, placeholder: Text("Full Name"), imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 32)
+                    
+                    CustomSecureField(text: $password, placeholder: Text("Passowrd"))
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 32)
+                    
+                }
+
+                
+                Button(action: {}, label: {
+                    Text("Sign in")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 50)
+                        .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                        .clipShape(Capsule())
+                        .padding()
+                })
+                
+                Spacer()
+                
+                
+                
+                Button(action: {
+                    mode.wrappedValue.dismiss()
+                }, label: {
+                    HStack {
+                        Text("Already have an account?")
+                            .font(.system(size: 14))
+                        
+                        Text("Sign In")
+                            .font(.system(size: 14, weight: .semibold))
+                    }.foregroundColor(.white)
+                })
+            
+            }
+            
+        }
     }
 }
 
