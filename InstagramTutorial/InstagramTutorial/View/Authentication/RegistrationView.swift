c108597a2f7a998e8bfb968f7aaf489590bee558
song1900 <woojin1900@gmail.com>
2021-08-01 23:33:11 +0900

    Uploading photo & saving user info in DB

--- InstagramTutorial/InstagramTutorial/View/Authentication/RegistrationView.swift
+++ InstagramTutorial/InstagramTutorial/View/Authentication/RegistrationView.swift
@@ -80,7 +80,7 @@ struct RegistrationView: View {
                 }
                 
                 Button(action: {
-                    viewModel.register(withEmail: email, password: password)
+                    viewModel.register(withEmail: email, password: password, image: selectedImage, fullname: fullname, username: username)
                 }, label: {
                     Text("Sign Up")
                         .font(.headline)
